dd36d5fd8b4f3677b56cfaeb31e320ea6b1d2335
huntersteffner <huntersteffner@gmail.com>
2024-12-26 17:01:14 -0500

    Mobile Navbar

---
+++ resources/js/Components/NavbarLinksDesktop.tsx
@@ -0,0 +1,13 @@
+import NavLink from "./NavLink"
+
+export default function NavbarLinksDesktop({ pageLinks }: {pageLinks: string[]}) {
+    return (
+        <div className="flex py-2">
+            {pageLinks.map((pageLink: string, i: number) => (
+                <div key={i} className='p-3 hover:font-bold'>
+                    <NavLink href={route(pageLink)} active={route().current(pageLink)}>{pageLink.charAt(0).toUpperCase() + pageLink.slice(1)}</NavLink>
+                </div>
+            ))}
+        </div>
+    )
+}
--- resources/js/Layouts/Navbar.tsx
+++ resources/js/Layouts/Navbar.tsx
@@ -1,27 +1,30 @@
+import NavbarLinksDesktop from "@/Components/NavbarLinksDesktop"
+import NavbarLinksMobile from "@/Components/NavbarLinksMobile"
 import NavLink from "@/Components/NavLink"
 import { User } from "@/types"
 import { Link } from "@inertiajs/react"
+import { useMediaQuery } from "react-responsive"
 
 export default function Navbar({user}: {user: User}) {
+
+    const pageLinks: string[] = [
+        'welcome',
+        'tutors',
+        'about',
+        'history'
+    ]
+
+    const isMobile: boolean = useMediaQuery({query: '(min-width: 720px)'})
     return (
-        <header className="bg-blue-800 text-white py-1">
-            <nav className="flex justify-between max-w-7xl items-center h-[4rem] gap-6 mx-auto">
+        <header className="bg-blue-800 text-white py-1 z-30">
+            <nav className="flex justify-between max-w-7xl items-center min-h-[4rem] gap-6 mx-auto bg-blue-800 z-30">
                 
-                <div className="flex py-2">
-                    <div className="p-3 hover:font-bold">
-                        <NavLink href={route('welcome')} active={route().current('welcome')}>Home</NavLink>
-                    </div>
-                    <div className="p-3 hover:font-bold">
-                        <NavLink href={route('tutors')} active={route().current('tutors')}>Tutors</NavLink>
-                    </div>
-                    <div className="p-3 hover:font-bold">
-                        <NavLink href={route('about')} active={route().current('about')}>About</NavLink>
-                    </div>
-                    <div className="p-3 hover:font-bold">
-                        <NavLink href={route('history')} active={route().current('history')}>Session History</NavLink>
-                    </div>
-                </div>
-                <div className="flex justify-end border border-white border-solid rounded px-4 py-2 text-center">
+                {isMobile ?    
+                    <NavbarLinksDesktop pageLinks={pageLinks} />
+                    :
+                    <NavbarLinksMobile pageLinks={pageLinks} />
+                }
+                <div className="flex justify-end border border-white border-solid rounded px-4 py-2 text-center z-30 bg-blue-800">
                     {user ? 
                         (
                             <div className="w-20 hover:font-bold">
